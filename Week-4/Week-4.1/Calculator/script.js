ff0e6bb89da1610309c32d79ebf4232132ca649a
Ritesh Giri <rrgiri032@gmail.com>
2024-05-23 13:53:37 +0530

    Principal Calculator

--- Week-4/Week-4.1/Calculator/script.js
+++ Week-4/Week-4.1/Calculator/script.js
@@ -1,9 +1,38 @@
+let timeout;
+function debnouncePopulateDiv() {
+  clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    populateDiv();
+  }, 1000);
+}
+
 async function populateDiv() {
   const a = parseInt(document.getElementById("one").value);
   const b = parseInt(document.getElementById("two").value);
-  //   const solution = (document.getElementById("finalSum").innerHTML = sum);
 
-  await fetch("https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b);
-
-  //   let sum = firstNum + secondNum;
+  await fetch("https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b).then(
+    function (response) {
+      console.log(response);
+      response.text().then(function (ans) {
+        console.log(ans);
+        document.getElementById("finalSum").innerHTML = ans;
+      });
+    }
+  );
 }
+
+// Another way to do this or we can say cleaner way.
+
+// async function populateDiv() {
+//   const a = parseInt(document.getElementById("one").value);
+//   const b = parseInt(document.getElementById("two").value);
+
+//   const response = await fetch(
+//     "https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b
+//   );
+//   const ans = await response.text();
+
+//   document.getElementById("finalSum").innerHTML = ans;
+//   console.log(ans);
+// }
