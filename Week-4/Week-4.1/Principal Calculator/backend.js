eaeaa26b24ca8ad6e1284195e0cf5a7f78c6f8b0
Ritesh Giri <rrgiri032@gmail.com>
2024-05-23 21:33:43 +0530

    Updated

--- Week-4/Week-4.1/Principal Calculator/backend.js
+++ Week-4/Week-4.1/Principal Calculator/backend.js
@@ -4,8 +4,11 @@ const port = 4000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.post("/calculate", (req, res) => {
-  const { principal, rate, time } = req.body;
+  const { principal, rate, time } = req.query;
 
   const principalVal = parseFloat(principal);
   const rateVal = parseFloat(rate);
@@ -19,7 +22,10 @@ app.post("/calculate", (req, res) => {
 
   const interest = (principalVal * rateVal * timeVal) / 100;
 
-  res.json({ principal: principalVal, interest: interest });
+  res.json({
+    principal: principalVal,
+    interest: interest,
+  });
 });
 
 app.listen(port, function () {
