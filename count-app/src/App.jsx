49225cbc05bbe69f03e2d58fbfcb0b267e918004
Ritesh Giri <rrgiri032@gmail.com>
2024-06-23 13:57:34 +0530

    Did some shit

--- count-app/src/App.jsx
+++ count-app/src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-zinc-700">
-      Hello world! Kya haal h sbhi k.
-    </h1>
+    <>
+      <div className="h-screen w-full flex justify-center items-center flex-col bg-zinc-700">
+        <h1 className="h-1/6 text-3xl font-bold text-white">
+          Hello world! Kya haal h sbhi k.
+        </h1>
+        <div className="h-1/6 w-96 mt-16 bg-black text-white text-center rounded-lg">
+          Kya haal h bhai k
+        </div>
+      </div>
+    </>
   );
 }
